a017bd666838f02e2481bc23c418582ac0fc1887
Jasper Zwiers <jasper.zwiers@hva.nl>
2018-02-15 10:06:46 +0100

    Project added + Launchscreen

---
+++ JuniorLeraar/JuniorLeraar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuniorLeraar
+//
+//  Created by Jasper Zwiers on 15-02-18.
+//  Copyright © 2018 Jasper Zwiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
